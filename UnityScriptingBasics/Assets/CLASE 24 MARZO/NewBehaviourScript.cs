522a326cfd08c64e2886697df48450ad77290668
DESKTOP-2GAD90N\MARIA CAMILA <camiladuram@hotmail.com>
2018-03-24 16:35:36 -0500

    El proyecto de Unity que modificamos en clase

---
+++ UnityScriptingBasics/Assets/CLASE 24 MARZO/NewBehaviourScript.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		//Unity lo ejecuta cada vez que empieza el playback. Solo se ejectuta 1 vez al principio
+		//void Start es una funcion, metodo o procedimiento
+		print ("One Shot"); //print solo funciona para escribir cosas en la consola
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		this.transform.Translate (0f, 0f, -1f);
+		//Lo ejecuta despues de hacer todo lo de Start. Es un ciclo que se renueva durante todo el juego. 
+
+		//Variable de tipo bool (solo puede ser verdadera o falsa) que la relaciona con la tecla de flecha hacia la derecha
+		bool isRightPressed = Input.GetKey (KeyCode.RightArrow); //click izq=0  click derecho=1  click medio=2
+		print (isRightPressed); //Para que se vea en la consola
+
+		//Para que se mueva a la derecha
+		if (isRightPressed) {
+			this.transform.Translate (-1, 0, 0);
+		}
+		//Para la IZQUIERDAAAA!!! 
+		bool isLeftPressed = Input.GetKey (KeyCode.LeftArrow);
+		if (isLeftPressed) {
+			this.transform.Translate (1, 0, 0);
+		}
+
+		//Para arriba
+		bool isUpPressed = Input.GetKey (KeyCode.UpArrow);
+		if (isUpPressed) {
+			this.transform.Translate (0, 1, 0);
+		}
+
+		//Para abajo
+		bool isDownPressed = Input.GetKey (KeyCode.DownArrow);
+		if (isDownPressed) {
+			this.transform.Translate (0, -1, 0);
+		}
+
+		//Rotacion para la IZQUIERDAAAA
+		bool isZPressed = Input.GetKey (KeyCode.Z);
+		if (isZPressed) {
+			this.transform.Rotate (0f, -0.5f, 0f);
+		}
+		//Rotacion para la derecha
+		bool isXPressed = Input.GetKey (KeyCode.X);
+		if (isXPressed) {
+			this.transform.Rotate (0f, 0.5f, 0f);
+		}
+	
+	}
+}
---
+++ UnityScriptingBasics/Assets/Scripts/Basics/Console.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Console : MonoBehaviour {
+
+	void Start () {
+		
+		/*DESDE AQUI ES LO QUE HICIMOS EN CLASE
+		 * //importante: este script solo sirve si el game object Console est[a activado en unity
+		//Pos para que sirva descomentar las instrucciones de aui abajo y hacer comentarios de las otras
+		Debug.Log ("BUENAS"); //aparece en la window "consola"
+		print ("buenas otra vez!"); //print hace lo mismo que Degug.log
+
+		Debug.LogWarning ("Oh no!");//aparece con el icono de advertencia
+		Debug.LogError ("No sirve D:");//aparece con el icono rojo de error
+		*/
+
+
+
+
+		// Impresion de Info en consola
+        Debug.Log("Este texto será mostrado en la consola, es muy util!");
+        print("Este texto también será impreso...");
+
+        // Impresion de un Warning en consola
+        Debug.LogWarning("Ha sucedido un una situacion no esperada");
+
+        // Impresion de un Error en consola
+        Debug.LogError("Ha sucedido un error");
+
+        // Impresion del contenido de una variable
+		//Pos saber que valor tiene la variable 
+        int edad = 25;
+        print("Mi edad es " + edad);
+
+        Vector3 ubicacion = Vector3.right;
+        print("Mi ubicación es " + ubicacion);
+        
+	}
+}
